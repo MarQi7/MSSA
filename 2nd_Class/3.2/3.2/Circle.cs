de9cd38b20ace61990e01cfe37e328c0ea8b8303
MarQi7 <deandrejackson16@gmail.com>
2024-05-21 16:51:39 -0400

    updates

---
+++ 2nd_Class/3.2/3.2/Circle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2
+{
+    internal class Circle
+    {
+        public double Radius { get; set; }
+        private double area;
+        public double Area
+        {
+            get { return area; }
+        }
+
+        private void GetRadius()
+        {
+            this.Radius = Math.Sqrt((this.area/Math.PI));
+        }
+        public double CalculateArea()
+        {
+            this.area = Math.PI * this.Radius * this.Radius;
+            return this.area;
+        }
+        public static Circle operator +(Circle c1, Circle c2) //creates a method for adding these objects with "+"
+        {
+            Circle cnew = new Circle();
+            cnew.area = c1.Area + c2.Area; // combines the two radius before calculating the area...
+            cnew.GetRadius();
+            return cnew;
+        }
+        public static Circle operator -(Circle c1, Circle c2)
+        {
+            Circle cnew = new Circle();
+            cnew.area = c1.Area - c2.Area; // combines the two radius before calculating the area...
+            cnew.GetRadius();
+            return cnew;
+        }
+        // if by radius:
+        // cnew.Radius = c1.Radius + c2.Radius;
+        // cnew.CalculateArea();
+    }
+}
---
+++ 2nd_Class/3.2/3.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Assignment 3.2: ");
+
+
+            Console.WriteLine("\nPart 1: Creating a visual Matrix");
+            
+            
+            Console.WriteLine("\nPart 2: Matrix Math");
+            
+            
+            Console.WriteLine("\nPart 3: Overload Operators");
+            
+            
+            Console.WriteLine("\nPart 4: Sum & Average Calculator");
+            
+            
+            Console.WriteLine("\nPart 5: ~IndexOf Function");
+            
+            
+            Console.Write("\n\nPress any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
